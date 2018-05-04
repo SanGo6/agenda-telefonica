ac05683f5aa0d89be04fb2c39bea64a6f3cdd5eb
Adrian Pic <al315544@uji.es>
2018-05-04 21:26:12 +0200

    Commit inicial.

---
+++ src/test/java/es/uji/ei1017/AppTest.java
@@ -0,0 +1,20 @@
+package es.uji.ei1017;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
